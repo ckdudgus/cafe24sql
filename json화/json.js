f03ac1b3b659b173539911f75e487f1701a2b66d
Cha Young Hyun <96039138+ckdudgus@users.noreply.github.com>
2022-05-10 13:01:58 +0900

    Delete json화 directory

--- json화/json.js
+++
@@ -1,5 +0,0 @@
-// 폼필드에 내가 원하는댈 사용자가 데이터를 삽입했음 
-var  jsonstr = decodeURIComponent($("[name='"+props.botable+"']").serialize());           
-var  json_form = JSON.stringify(jsonstr).replace(/\&/g, '\",\"') 
-json_form = "{\""+ json_form.replace(/=/gi, '\":\"') + "\"}"
-// json화시킴 -> 노드
